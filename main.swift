3ce19f8403381f92e27f2f0e3c3ce44d6e538730
liujinlongxa <liujinlongxa@gmail.com>
2019-07-13 00:27:18 +0800

    add MinHeap

--- main.swift
+++ main.swift
@@ -19,5 +19,17 @@ func testMaxHeap() {
     assert(data.extractMax() == 13)
 }
 
+func testMinHeap() {
+    var data = MinHeap<Int>()
+    data.add(element: 12)
+    data.add(element: 13)
+    data.add(element: 3)
+    data.add(element: 133)
+    assert(data.extractMin() == 3)
+    data.add(element: 4)
+    assert(data.extractMin() == 4)
+}
+
 testMaxHeap()
+testMinHeap()
 
