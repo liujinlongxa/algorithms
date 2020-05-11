403b978f103194a4c999f0cc67d017fec02e8af7
liujinlong <jl.liu@lazyaudio.com>
2020-05-12 06:45:01 +0800

    finish some problem

--- New/Problem05.swift
+++ New/Problem05.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+func replaceSpace(_ s: String) -> String {
+    let chars = Array(s)
+    var newChars = Array<Character>(repeating: "0", count: chars.count * 3)
+    var size = 0
+    for (idx, c) in chars.enumerated() {
+        if c == " " {
+            newChars[size] = "%"
+            newChars[size + 1] = "%"
+            newChars[size + 2] = "%"
+            size += 3
+        } else {
+            newChars[size] = Character(extendedGraphemeClusterLiteral: c)
+            size += 1
+        }
+    }
+    
+    return String(newChars[...size])
+}
+
+func replaceSpace2(_ s: String) -> String {
+    return s.replacingOccurrences(of: " ", with: "%20")
+}
