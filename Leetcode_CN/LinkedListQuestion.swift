b96d0a316b0d654a4a55ca94fe272cec01d85341
liujinlongxa <liujinlongxa@gmail.com>
2020-06-03 06:23:23 +0800

    update

--- Leetcode_CN/LinkedListQuestion.swift
+++ Leetcode_CN/LinkedListQuestion.swift
@@ -119,4 +119,78 @@ struct LinkedListQuestion {
         
         return dummyHead.next
     }
+    
+    /// 2. 两数相加
+    /// https://leetcode-cn.com/problems/add-two-numbers/
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var newList: ListNode?
+
+        var temp1 = l1, temp2 = l2
+        var flag = 0 // 进位
+        while temp1 != nil || temp2 != nil {
+            let num1 = temp1?.val ?? 0
+            let num2 = temp2?.val ?? 0
+            let sum = num1 + num2 + flag
+            if sum >= 10 {
+                let newNodeVal = sum % 10
+                temp1?.val = newNodeVal
+                temp2?.val = newNodeVal
+                flag = 1
+            } else {
+                temp1?.val = sum
+                temp2?.val = sum
+                flag = 0
+            }
+            
+            if temp1?.next == nil && temp2?.next == nil && flag == 1 {
+                if temp1 != nil {
+                    temp1?.next = ListNode(1)
+                } else {
+                    temp2?.next = ListNode(1)
+                }
+                flag = 0
+            }
+            
+            newList = temp1 == nil ? l2 : l1
+            temp1 = temp1?.next
+            temp2 = temp2?.next
+        }
+        
+        return newList
+    }
+    
+    /// 21. 合并两个有序链表
+    /// https://leetcode-cn.com/problems/merge-two-sorted-lists/
+    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        guard let l1 = l1 else {
+            return l2
+        }
+        
+        guard let l2 = l2 else {
+            return l1
+        }
+        
+        var temp1: ListNode? = l1
+        var temp2: ListNode? = l2
+        let ret: ListNode? = ListNode(0) // 头结点
+        var newList = ret
+        
+        while temp1 != nil && temp2 != nil {
+            var val1 = (temp1?.val)!
+            var val2 = (temp2?.val)!
+            if val1 > val2 {
+                newList?.next = ListNode(val2)
+                temp2 = temp2?.next
+            } else {
+                newList?.next = ListNode(val1)
+                temp1 = temp1?.next
+            }
+            newList = newList?.next
+            
+        }
+        
+        newList?.next = temp1 == nil ? temp2 : temp1
+        
+        return ret?.next
+    }
 }
